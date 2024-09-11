93c277dd6d0031fabd4124f300130a389e1ead73
arunjanardanan8m <arunjanardanan8@gmail.com>
2024-09-11 17:16:30 +0530

    Initial commit

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("Hello, World!");
+//console.log(a); // undefined due to hoisting
+let a = 10;
+console.log(a) // 10
+function name(params,name) {
+    console.log(name)
+}
+name(10,"drttt")
